afabae87b79ab615937b9592ff1967f9144a6fd7
3cordguy <jweaver911@gmail.com>
2023-09-30 10:09:35 -0400

    improves generic collision func

--- src/collisions.lua
+++ src/collisions.lua
@@ -51,7 +51,16 @@ end
 -- Returns true if two boxes overlap, false if they don't;
 -- x1,y1 are the top-left coords of the first box, while w1,h1 are its width and height;
 -- x2,y2,w2 & h2 are the same, but for the second box.
-function check_collision(x1,y1,w1,h1, x2,y2,w2,h2)
+function check_collision(o, p)
+  local x1 = o.x
+  local y1 = o.y
+  local w1 = o.w
+  local h1 = o.h
+  local x2 = p.x
+  local y2 = p.y
+  local w2 = p.w
+  local h2 = p.h
+
   return x1 < x2+w2 and
          x2 < x1+w1 and
          y1 < y2+h2 and
