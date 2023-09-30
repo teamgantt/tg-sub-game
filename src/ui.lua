b239fa8800826bac7384d9631ac66e013e470ea9
3cordguy <jweaver911@gmail.com>
2023-09-30 10:10:25 -0400

    improves hud

--- src/ui.lua
+++ src/ui.lua
@@ -7,8 +7,9 @@ function u_ui()
 end
 
 function d_ui()
+  local x_off=18 --bar offsets
+  local y_off=2
   -- sub hull
-  print('hull', cam.x+2, cam.y+107, 7)
   local hull_color = 11 -- good
 
   if (tgsub.hull < 66 and tgsub.hull > 33) then
@@ -17,15 +18,15 @@ function d_ui()
     hull_color = 8 -- bad
   end
 
-  line(cam.x+2, cam.y+114, cam.x+tgsub.hull, cam.y+114, hull_color)
-  rect(cam.x+2, cam.y+113, cam.x+100, cam.y+115, 7)
+  -- hull
+  print('HULL', cam.x+2, cam.y+120, 10)
+  line(cam.x+2+x_off, cam.y+121+y_off, cam.x+tgsub.hull+x_off, cam.y+121+y_off, hull_color)
+  rect(cam.x+2+x_off, cam.y+120+y_off, cam.x+100+x_off, cam.y+122+y_off, 7)
 
   if (player.mode == 'diver') then
     -- diver o2
-    print('o2', cam.x+2, cam.y+118, 7)
-
-    line(cam.x+12, cam.y+121, cam.x+player.diver.o2, cam.y+121, 12)
-    rect(cam.x+12, cam.y+120, cam.x+60, cam.y+122, 7)
-    -- rectfill(cam.x+12, cam.y+120, cam.x+player.diver.o2, cam.y+122, 12)
+    print('AIR', cam.x+2, cam.y+115, 12)
+    line(cam.x+3+x_off, cam.y+116+y_off, cam.x+player.diver.o2+x_off, cam.y+116+y_off, 12)
+    rect(cam.x+2+x_off, cam.y+115+y_off, cam.x+60+x_off, cam.y+117+y_off, 7)
   end
 end
